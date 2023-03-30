5f09dc862fc5d6da4f9df9b0efbcc0db6e249abe
thh <hzjmessage@outlook.com>
2023-03-30 09:57:40 +0800

    master:unicodeDecode

--- test/rune_test.go
+++ test/rune_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"testing"
 	"unicode/utf8"
 )
@@ -17,5 +19,23 @@ func TestRune(t *testing.T) {
 	fmt.Println(string([]rune{20320, 22909}))
 	fmt.Println(utf8.RuneCountInString("你好n"))
 	fmt.Println(len([]rune("你好n")))
-	
+
+}
+
+func TestUnicode(t *testing.T) {
+
+	str := "Hello, \\u4e2d\\u6587! 42"
+	// 输出转换后的字符串
+	fmt.Println(unicodeDecode(str))
+
+}
+func unicodeDecode(str string) string {
+	// 正则表达式，用于匹配Unicode编码部分
+	re := regexp.MustCompile(`\\u[0-9a-fA-F]{4}`)
+
+	// 将Unicode编码转换为中文字符串
+	return re.ReplaceAllStringFunc(str, func(m string) string {
+		code, _ := strconv.ParseInt(m[2:], 16, 32)
+		return string(rune(code))
+	})
 }
