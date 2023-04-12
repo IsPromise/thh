8ef69e530e7c752c846515652b151e67881d16fc
thh <hzjmessage@outlook.com>
2023-04-13 07:36:13 +0800

    master:tcp_scan

--- actor/src/pages/home/bbs/ArticlesPage.vue
+++ actor/src/pages/home/bbs/ArticlesPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {NCard, NList, NListItem, NSpace, NTag, NThing} from 'naive-ui'
+import {NCard, NH2, NLayout, NLayoutContent, NLayoutSider, NList, NListItem, NSpace, NTag, NThing} from 'naive-ui'
 import {onMounted, ref} from "vue";
 import {useRouter} from "vue-router";
 import {getArticlesDetailApi} from "@/service/remote";
@@ -54,26 +54,58 @@ onMounted(() => {
 
 </script>
 <template>
-    <n-card style="margin:0 auto">
 
-        <h2> {{ articleInfo.title }}</h2>
+    <n-layout>
+        <n-layout has-sider sider-placement="right"
+                  class="content"
+                  :style="{flex: 1, maxWidth: '1400px', margin: '0 auto', padding: '24px'}">
+            <n-layout-content content-style="padding: 24px;">
 
-        <n-space size="small" style="margin-bottom: 16px">
-            <n-tag v-for="itemTag in articleInfo.tag" :bordered="false" type="info" size="small"
-                   v-text="itemTag">
-            </n-tag>
-        </n-space>
 
-        <articles-md-page :markdown="articleInfo.body"></articles-md-page>
+                <n-card style="margin:0 auto">
 
-    </n-card>
-    <n-card style="margin:0 auto">
-        <n-list>
-            <n-list-item v-for="item in commentList">
-                <n-thing :title="item.userId" content-style="margin-top: 10px;">
-                    <articles-md-page :markdown="item.content"></articles-md-page>
-                </n-thing>
-            </n-list-item>
-        </n-list>
-    </n-card>
+                    <h2> {{ articleInfo.title }}</h2>
+
+                    <n-space size="small" style="margin-bottom: 16px">
+                        <n-tag v-for="itemTag in articleInfo.tag" :bordered="false" type="info" size="small"
+                               v-text="itemTag">
+                        </n-tag>
+                    </n-space>
+
+                    <articles-md-page :markdown="articleInfo.body"></articles-md-page>
+
+                </n-card>
+                <n-card style="margin:0 auto">
+                    <n-list>
+                        <n-list-item v-for="item in commentList">
+                            <n-thing :title="item.userId" content-style="margin-top: 10px;">
+                                <articles-md-page :markdown="item.content"></articles-md-page>
+                            </n-thing>
+                        </n-list-item>
+                    </n-list>
+                </n-card>
+
+
+            </n-layout-content>
+            <n-layout-sider
+                    :width="360"
+                    content-style="padding: 24px;"
+                    bordered
+            >
+                <n-card>
+                    <n-h2>海淀桥biubiubiu</n-h2>
+                </n-card>
+
+            </n-layout-sider>
+        </n-layout>
+    </n-layout>
 </template>
+
+
+<style>
+@media (max-width: 1200px) {
+    .content {
+        max-width: 100%;
+    }
+}
+</style>
--- actor/src/pages/home/bbs/BBSIndex.vue
+++ actor/src/pages/home/bbs/BBSIndex.vue
@@ -11,37 +11,5 @@ const message = useMessage()
 </script>
 
 <template>
-    <n-layout>
-        <n-layout has-sider sider-placement="right"
-                  class="content"
-                  :style="{flex: 1, maxWidth: '1400px', margin: '0 auto', padding: '24px'}">
-            <n-layout-content content-style="padding: 24px;">
-                <router-view></router-view>
-            </n-layout-content>
-            <n-layout-sider
-                    :width="360"
-                    content-style="padding: 24px;"
-                    bordered
-            >
-                <n-card>
-                    <n-h2>海淀桥</n-h2>
-                </n-card>
-
-                <n-card>
-                    <n-h2>海淀桥</n-h2>
-                </n-card>
-
-                <n-card>
-                    <n-h2>海淀桥</n-h2>
-                </n-card>
-            </n-layout-sider>
-        </n-layout>
-    </n-layout>
+    <router-view></router-view>
 </template>
-<style>
-@media (max-width: 1200px) {
-    .content {
-        max-width: 100%;
-    }
-}
-</style>
