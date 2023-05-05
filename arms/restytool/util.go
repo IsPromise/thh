22c0ab2fe38dd97524755e402f1fb9c2078c3ca0
thh <hzjmessage@outlook.com>
2023-05-05 23:01:01 +0800

    add:resty 2 curl

--- arms/restytool/util.go
+++ arms/restytool/util.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/go-resty/resty/v2"
 
@@ -80,4 +82,41 @@ func GetCurlByR(r resty.Response) bytes.Buffer {
 	}
 	b2.WriteString(" --compressed --insecure ")
 	return b2
+}
+
+func GenerateCurlCommand(resp *resty.Response) string {
+	var cmd strings.Builder
+
+	cmd.WriteString(fmt.Sprintf("curl '%v' -X '%v'", resp.Request.URL, resp.Request.Method))
+
+	// 处理请求头信息
+	for k, v := range resp.Request.Header {
+		if k == "Content-Type" {
+			cmd.WriteString(fmt.Sprintf(" -H '%v:%v'", k, v[0]))
+		} else {
+			cmd.WriteString(fmt.Sprintf(" -H '%v:%v'", k, strings.Join(v, ",")))
+		}
+	}
+
+	// 处理请求体信息
+	if resp.Request.Body != nil {
+		bodyBytes, _ := json.Marshal(resp.Request.Body)
+		if len(bodyBytes) > 0 {
+			cmd.WriteString(fmt.Sprintf(" --data-raw '%v'", string(bodyBytes)))
+		}
+	}
+
+	// 处理表单数据信息
+	if len(resp.Request.FormData) > 0 {
+		formValues := url.Values{}
+		for key, value := range resp.Request.FormData {
+			formValues.Set(key, value[0])
+		}
+		cmd.WriteString(fmt.Sprintf(" '%v'", formValues.Encode()))
+	}
+
+	// 添加其他参数信息
+	cmd.WriteString(" --compressed --insecure ")
+
+	return cmd.String()
 }
