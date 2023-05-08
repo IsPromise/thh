2ae104353d6a969b3aedfcb35da47a77bb1eb3ed
thh <hzjmessage@outlook.com>
2023-05-08 22:49:57 +0800

    master:log iplimit

--- app/http/middleware/gin_ip_limit.go
+++ app/http/middleware/gin_ip_limit.go
@@ -4,12 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/leancodebox/goose/serverinfo"
 	"net/http"
+	"thh/bundles/logging"
 )
 
 func IpLimit(c *gin.Context) {
 	clientIP := c.ClientIP()
 	ip, _ := serverinfo.GetLocalIp()
 	if len(ip) != 0 && clientIP != ip && clientIP != "::1" {
+		logging.Error("clientIp:"+clientIP, " localIp:"+ip)
 		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Access denied"})
 		return
 	}
