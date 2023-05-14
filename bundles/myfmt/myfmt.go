f54d94a8839191405512be2cf953bea0dc2fa013
thh <hzjmessage@outlook.com>
2023-05-14 22:47:36 +0800

    master:fix:root

--- bundles/myfmt/myfmt.go
+++ bundles/myfmt/myfmt.go
@@ -2,7 +2,9 @@ package myfmt
 
 import (
 	"fmt"
+	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func PrintlnWithCaller(v ...interface{}) {
@@ -27,7 +29,28 @@ func PrintlnWithCaller(v ...interface{}) {
 		}
 	}
 
+	// 获取项目根目录路径
+	rootDir := getRootDir()
+
+	// 简化文件名，只保留项目内的路径部分
+	file = strings.TrimPrefix(file, rootDir)
+	file = strings.TrimPrefix(file, "/")
+
 	// 打印消息
-	fmt.Printf("[%s:%d] %s: ", file, line, packageName)
+	fmt.Printf("%s:%d %s: ", file, line, packageName)
 	fmt.Println(v...)
+}
+
+func getRootDir() string {
+	_, file, _, ok := runtime.Caller(0)
+	if !ok {
+		file = "???"
+		return ""
+	}
+
+	for i := 0; i < 3; i++ {
+		file = filepath.Dir(file)
+	}
+
+	return file
 }
