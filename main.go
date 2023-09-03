06f070286dacc60ab727795461e7af8bf7dd1e29
thh <hzjmessage@outlook.com>
2023-09-03 21:15:42 +0800

    master:fix name

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"embed"
 	_ "net/http/pprof"
-	kernel2 "thh/app/bundles/kernel"
+	"thh/app/bundles/kernel"
 	"thh/app/console"
 )
 
@@ -15,8 +15,8 @@ var oConfig string
 
 func main() {
 	// 注册静态资源
-	kernel2.InitStart()
-	kernel2.ActorSave(actorFS)
-	kernel2.SetOConfig(oConfig)
+	kernel.InitStart()
+	kernel.ActorSave(actorFS)
+	kernel.SetOConfig(oConfig)
 	console.Execute()
 }
